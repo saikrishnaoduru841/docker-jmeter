df8930afd766699b7ecf916950cecfe6be31fc24
saikrishnaoduru841 <89731382+saikrishnaoduru841@users.noreply.github.com>
2023-01-24 12:24:42 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,10 +5,10 @@
 # Example for using User Defined Variables with JMeter
 # These will be substituted in JMX test script
 # See also: http://stackoverflow.com/questions/14317715/jmeter-changing-user-defined-variables-from-command-line
-export TARGET_HOST="www.map5.nl"
+export TARGET_HOST="www.google.com"
 export TARGET_PORT="80"
-export TARGET_PATH="/kaarten.html"
-export TARGET_KEYWORD="Kaartdiensten"
+export TARGET_PATH="/index.html"
+export TARGET_KEYWORD="docker"
 
 T_DIR=tests/trivial
 
